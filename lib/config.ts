02d03cd0cc17ec56effa208e97d70f14f77488f2
unknown <asifjamil468@gmail.com>
2026-07-13 14:35:27 +0530

    Milestone 1 - Foundation

---
+++ lib/config.ts
@@ -0,0 +1,15 @@
+export const APP_CONFIG = {
+  appName: "Snow",
+
+  husband: {
+    name: "Asif Jamil",
+    birthday: "1992-07-11",
+  },
+
+  wife: {
+    name: "Shanowar Khatoon",
+    birthday: "1998-12-06",
+  },
+
+  marriageDate: "2026-06-07",
+};
